23618284e2611a2aa31d6855b3108e6b8ed2e1ab
Rogério <rogerio.souza@skeelo.app>
2022-01-09 15:28:04 -0300

    refact: added terminal controller to isolate terminal functions

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readline from 'readline'
-
 import database from '../database.json'
-import Person from './person.js'
+import TerminalController from './controllers/terminal.js'
 
-const DEFAULT_LANGUAGE = "pt-BR"
-DraftLog(console).addLineListener(process.stdin)
+const DEFAULT_LANG = "pt-BR"
 
-const options = {
-    leftPad: 2,
-    columns: [
-        { field: "id", name: chalk.cyan("ID") },
-        { field: "vehicles", name: chalk.green("Vehicles") },
-        { field: "kmTraveled", name: chalk.magenta("KM Traveled") },
-        { field: "from", name: chalk.white("From") },
-        { field: "to", name: chalk.yellow("TO") }
-    ]
-}
+const terminalController = new TerminalController()
 
-const table = chalkTable(options, database.map(item => new Person(item).formated(DEFAULT_LANGUAGE)))
-const print = console.draft(table)
-
-const terminal = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-terminal.question('What`s your name?', msg => {
-    console.log(msg)
-})
+terminalController.initializeTerminal(database, DEFAULT_LANG)
